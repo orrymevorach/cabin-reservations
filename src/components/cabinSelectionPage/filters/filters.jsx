77e3421fc4d12aba401325f98ac3efd4ef5e4853
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2024-08-02 11:09:32 -0400

    add filter query params

--- src/components/cabinSelectionPage/filters/filters.jsx
+++ src/components/cabinSelectionPage/filters/filters.jsx
@@ -3,20 +3,42 @@ import styles from './filters.module.scss';
 import { useCabinAndUnitData } from '@/context/cabin-and-unit-data-context';
 import clsx from 'clsx';
 import { FILTERS, useFilters } from './filters-context';
-import CategoryButtons from './category-buttons';
+import { getFilterCategories } from '../units/unitRow/cabinList/filter-utils';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function Filters({ classNames = '' }) {
   const { selectedFilters, setSelectedFilters } = useFilters();
   const { units } = useCabinAndUnitData();
+  const router = useRouter();
 
   const numberOfGuests = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   const unitNames = units.map(({ name }) => name);
+  const categories = getFilterCategories({ unitData: units });
 
   const handleChange = ({ event, label }) => {
     const selectedFiltersCopy = Object.assign({}, selectedFilters);
     selectedFiltersCopy[label] = event;
     setSelectedFilters(selectedFiltersCopy);
+    router.push(
+      {
+        pathname: '/cabin-selection',
+        query: {
+          ...router.query,
+          [label]: event,
+        },
+      },
+      undefined,
+      { shallow: true }
+    );
   };
+
+  // On page load, set filters based on filter query params
+  useEffect(() => {
+    if (router.query) {
+      setSelectedFilters(router.query);
+    }
+  }, []);
 
   return (
     <div className={clsx(styles.filters, classNames)}>
@@ -33,8 +55,12 @@ export default function Filters({ classNames = '' }) {
         variant="standard"
         handleChange={event => handleChange({ event, label: 'Unit' })}
       />
-
-      <CategoryButtons handleChange={handleChange} />
+      <Dropdown
+        options={['All', ...categories]}
+        label={FILTERS.CATEGORY}
+        variant="standard"
+        handleChange={event => handleChange({ event, label: 'Category' })}
+      />
     </div>
   );
 }
--- src/components/cabinSelectionPage/units/unitRow/cabinList/filter-utils.js
+++ src/components/cabinSelectionPage/units/unitRow/cabinList/filter-utils.js
@@ -3,8 +3,9 @@ import { FILTERS } from '../../../filters/filters-context';
 const { AVAILABLE_BEDS } = FILTERS;
 
 export const filterByCategory = ({ cabins, selectedFilters }) => {
+  const categoryFilterSelection = selectedFilters[FILTERS.CATEGORY];
+  if (!categoryFilterSelection) return cabins;
   return cabins.filter(({ category }) => {
-    const categoryFilterSelection = selectedFilters[FILTERS.CATEGORY];
     if (!categoryFilterSelection) return true;
     if (category && category[0] && categoryFilterSelection === category[0])
       return true;
@@ -14,6 +15,7 @@ export const filterByCategory = ({ cabins, selectedFilters }) => {
 
 export const filterByAvailableBeds = ({ cabins, selectedFilters }) => {
   const availableBedsFilter = selectedFilters[AVAILABLE_BEDS];
+  if (!availableBedsFilter) return cabins;
   return cabins.filter(({ openBeds, availability }) => {
     if (
       availableBedsFilter &&
