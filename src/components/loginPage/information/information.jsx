858184315e695fe409100e4f2989187c7d589304
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2024-08-27 22:35:52 -0400

    reorder homepage information

--- src/components/loginPage/information/information.jsx
+++ src/components/loginPage/information/information.jsx
@@ -3,25 +3,26 @@ import styles from './information.module.scss';
 export default function Information() {
   return (
     <div className={styles.container}>
-      <p>Welcome to the reservation platform for Highlands Music Festival.</p>
-      <p>Here you are able to:</p>
-      <ol className={styles.list}>
-        <li>Book a cabin you would like to sleep in</li>
-        <li>Reserve a specific bed in your cabin</li>
-        <li>Make a reservation on behalf of your friends/partner</li>
-        <li>Make changes to an existing reservation</li>
-      </ol>
+      <p className={styles.title}>
+        Welcome to the reservation platform for Highlands Music Festival.
+      </p>
       <p>What you need to make a reservation:</p>
       <ol className={styles.list}>
         <li>Your email address</li>
         <li>
-          Your password, which you have received in an email once you purchased
-          a&nbsp;ticket.
+          Your password, which was shared when you received the booking email.
         </li>
         <li>
           The email address(es) for each person you wish to reserve a spot for
           (optional)
         </li>
+      </ol>
+      <p>Here you are able to:</p>
+      <ol className={styles.list}>
+        <li>Book a cabin you would like to sleep in</li>
+        <li>Make a reservation on behalf of your friends/partner</li>
+        <li>Reserve a specific bed in your cabin</li>
+        <li>Make changes to an existing reservation</li>
       </ol>
     </div>
   );
