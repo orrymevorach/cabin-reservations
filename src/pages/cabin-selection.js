97bfa152897e54003f9949817798fee7f165dd33
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2024-11-14 14:56:59 -0500

    close cabin selection page

--- src/pages/cabin-selection.js
+++ src/pages/cabin-selection.js
@@ -7,8 +7,20 @@ import { ReservationProvider } from '@/context/reservation-context';
 import { CabinCategoriesProvider } from '@/context/cabin-categories';
 import { UserProvider } from '@/context/user-context';
 import VisibleSectionProvider from '@/context/visible-section-context';
+import Takeover from '@/components/shared/takeover/takeover';
+import Button from '@/components/shared/button/button';
+import { ROUTES } from '@/utils/constants';
 
 export default function CabinSelection() {
+  return (
+    <Takeover hideCloseButton>
+      <p style={{ marginBottom: '20px' }}>
+        Cabin selection is not currently available. We will send out an email to
+        all ticket holders when cabin reservations open up.
+      </p>
+      <Button href={ROUTES.SUMMARY}>Reservation Summary</Button>
+    </Takeover>
+  );
   return (
     <VisibleSectionProvider>
       <CabinAndUnitDataProvider>
